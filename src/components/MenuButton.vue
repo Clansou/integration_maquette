24c150a9213cea4914b8b8e6b8056e12c62ffde0
Clansou <clement.souplet78@gmail.com>
2023-02-15 18:03:26 +0100

    Create sidebar

---
+++ src/components/MenuButton.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  props: ['img','text']
+}
+</script>
+
+<template>
+  <div class="MenuButton">
+    <img :src="img" alt=''>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<style scoped>
+    .MenuButton{
+        display: grid;
+        grid-template-columns: 2fr 4fr;
+        align-items: center;
+        padding: 0px 15px;
+        gap: 16px;
+        font-size: 25px;
+        font-weight: 500;
+        line-height: 38px;
+        letter-spacing: 0em;
+
+        
+    }
+    img{
+        height: 38px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,113 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import MenuButton from '@/components/MenuButton.vue';
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    MenuButton
+  },
 }
 </script>
+
+<template>
+  <aside>
+    <img id="Logo" src="../../public/image/Logo.png" alt="Logo">
+    <div id="Dashboard">
+      <img src="../../public/image/Icone/Dashboard.png" alt="">
+      <p>Dashboard</p>
+    </div>
+    <div id="Menu">
+      <MenuButton img="image/Icone/Invoices.png" text="InVoices" />
+      <MenuButton img="image/Icone/Clients.png" text="Clients" />
+      <MenuButton img="image/Icone/Products.png" text="Products" />
+      <MenuButton img="image/Icone/Messages.png" text="Messages" />
+      <MenuButton img="image/Icone/Settings.png" text="Settings" />
+      <MenuButton img="image/Icone/Help.png" text="Help" />
+      <MenuButton class="logout" img="image/Icone/LogOut.png" text="Log Out" />
+    </div>
+    
+  </aside>
+  
+  
+  <div>
+
+  </div>
+  <!-- <header>
+      <h1>Hi, Rogers</h1>
+      <div id="search">
+        <img src="../../public/image/Icone/Search.png" alt="">
+        <p>Search</p>
+      </div>
+  </header> -->
+</template>
+
+
+<style>
+  body{
+    background-color: #1E1E1E;
+    color: #FFFFFF;
+  }
+  #app{
+    display: grid;
+    grid-template-columns: 3fr 8fr 4fr ;
+    padding-top: 54px;
+  }
+  #Logo{
+    margin-bottom: 128px;
+  }
+  #app>aside{
+    margin-left: 52px;
+  }
+  #Dashboard{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 13px 29px;
+    gap: 17px;
+    border-radius:17px;
+    background: linear-gradient(266.37deg, #FAFF00 -5.82%, #FAFF00 15.95%, #1970D6 41.2%, #6C1182 72.88%);
+    margin-bottom: 44px;
+  }
+  #Dashboard>p{
+    font-weight: 400;
+    font-size: 25px;
+    line-height: 38px;
+    color: #000;
+  }
+  #Menu{
+    color: rgba(255, 255, 255, 0.8)
+    
+  }
+  .logout{
+    position: relative;
+    bottom: 0;
+  }
+  header{
+    display: flex;
+    margin-left: 12px;
+    margin-top: 30px;
+    align-items: center;
+    justify-content: space-between;
+    width: 70%;
+    height: 93px;
+  }
+  header>img{
+    margin-bottom: 10px;
+  }
+  h1{
+    font-weight: 500;
+    font-size: 35px;
+    line-height: 52px;
+  }
+  #search{
+    display: flex;
+    width: 37%;
+    flex-direction: row;
+    align-items: center;
+    padding-left: 24px;
+    gap: 10px;
+    background: #FFFFFF;
+    border-radius: 15px;
+    color: #000;
+    }
+</style>
