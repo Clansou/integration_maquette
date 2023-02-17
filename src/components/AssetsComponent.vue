e164f8ab20b201ca0535f296e86d7ac405db7b0c
Clansou <clement.souplet78@gmail.com>
2023-02-17 14:38:26 +0100

    Create Assets component

---
+++ src/components/AssetsComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  props: ['img','name','quantity','subname','background']
+}
+</script>
+<template>
+    <div>
+        <img :src="img" alt="">
+        <p>{{ name }}</p>
+        <div>
+            <p>{{ quantity }}</p>
+            <p>{{ subname }}</p>
+        </div>
+    </div>
+</template>
